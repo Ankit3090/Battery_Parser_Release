e850214ad4ce78bedd67f806e3da9ae432fef4ea
ankit <ankit023090@gmail.com>
2026-06-01 13:11:16 +0530

    Production Code\Temp files error still exists but auto-updating is happening

--- ParserApp/main.py
+++ ParserApp/main.py
@@ -19,7 +19,7 @@ import time
 
 # --- 1. CONFIGURATION ---
 # Increment this when you are ready to publish a new version on GitHub
-CURRENT_VERSION = "v1.9" 
+CURRENT_VERSION = "v1.0" 
 # Format: "YourGitHubUsername/YourRepoName" 
 REPO = "Ankit3090/Battery_Parser_Release" 
 
@@ -1233,10 +1233,10 @@ def generate_summary(df, target_dir):
         "Min AUX_SoC (%)": [min_aux_val_soc],
         "PCB Temperature MIN (deg C)": [pcb_temp_min_val],
         "PCB Temperature MAX (deg C)": [pcb_temp_max_val],
-        #"PCB Temp Delta on Same Instance (deg C)": [pcb_temp_delta_val],
-        #"All Error Efuse Validation": [all_errors_val],
-        #"Vehicle State": [vehicle_state_val],
-        #"STARK F/W": [stark_fw_val]
+        "PCB Temp Delta on Same Instance (deg C)": [pcb_temp_delta_val],
+        "All Error Efuse Validation": [all_errors_val],
+        "Vehicle State": [vehicle_state_val],
+        "STARK F/W": [stark_fw_val]
     }
     
     summary_df = pd.DataFrame(summary_data_horizontal)
