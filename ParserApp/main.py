8deea5f571156e0e9d01d9d6c74c564d9ec7e828
ankit <ankit023090@gmail.com>
2026-06-01 08:48:08 +0530

    Testing the auto-updater UI

--- ParserApp/main.py
+++ ParserApp/main.py
@@ -19,7 +19,7 @@ import time
 
 # --- 1. CONFIGURATION ---
 # Increment this when you are ready to publish a new version on GitHub
-CURRENT_VERSION = "v1.8.3" 
+CURRENT_VERSION = "v1.0" 
 # Format: "YourGitHubUsername/YourRepoName" 
 REPO = "Ankit023090/Battery_Parser_Release" 
 
@@ -1232,7 +1232,7 @@ def generate_summary(df, target_dir):
         "PCB Temp Delta on Same Instance (deg C)": [pcb_temp_delta_val],
         "All Error Efuse Validation": [all_errors_val],
         "Vehicle State": [vehicle_state_val],
-        #"STARK F/W": [stark_fw_val]
+        "STARK F/W": [stark_fw_val]
     }
     
     summary_df = pd.DataFrame(summary_data_horizontal)
