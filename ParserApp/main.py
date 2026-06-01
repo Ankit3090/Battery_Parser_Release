9740451e65f4aed2cd2109ed63b965ca33e3fc23
ankit <ankit023090@gmail.com>
2026-06-01 12:27:48 +0530

    Temp files error still exists but auto-updating is happening

--- ParserApp/main.py
+++ ParserApp/main.py
@@ -19,7 +19,7 @@ import time
 
 # --- 1. CONFIGURATION ---
 # Increment this when you are ready to publish a new version on GitHub
-CURRENT_VERSION = "v1.7" 
+CURRENT_VERSION = "v1.8" 
 # Format: "YourGitHubUsername/YourRepoName" 
 REPO = "Ankit3090/Battery_Parser_Release" 
 
